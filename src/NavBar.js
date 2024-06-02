006821da97c341505d7c9f11ab53dcab21cd4d06
Aayush Khopade <khopadeayush96@gmail.com>
2024-06-02 22:27:09 +0530

    final deploy

---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css';
+function NavBar(){
+    return(
+        <div class="navbar">
+            <div class="nav-info">
+                <h1>AAYUSH KHOPADE
+                    
+                </h1>
+            </div>
+            <div class="nav-items">
+                <a class="link" href="#home" id='smooth'>Home</a>
+                <a class="link" href="#skills" id='smooth'>Skills</a>
+                <a class="link" href="#project" id='smooth'>Projects</a>
+                <a class="link" href="#contact" id='smooth'>Contact me</a>
+            </div>
+        </div>
+    )
+}
+export default NavBar;
